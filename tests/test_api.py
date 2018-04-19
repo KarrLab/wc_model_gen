6297500ed326fb73f2053f860198d9799c2e1023
Jonathan Karr <jonrkarr@gmail.com>
2018-04-19 18:30:25 -0400

    adding tests for API

---
+++ tests/test_api.py
@@ -0,0 +1,16 @@
+""" Tests of the API
+
+:Author: Jonathan Karr <jonrkarr@gmail.com>
+:Date: 2018-04-19
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+
+import types
+import unittest
+import wc_model_gen
+
+
+class ApiTestCase(unittest.TestCase):
+    def test(self):
+        self.assertIsInstance(wc_model_gen.ModelGenerator, type)
