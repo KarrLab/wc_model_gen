9c2c84754b7cb2fa19ed827e40a5010b2bb701c7
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-07-05 14:09:15 -0400

    PEP 8 Formating

--- wc_model_gen/rand_gen/rna_degradation.py
+++ wc_model_gen/rand_gen/rna_degradation.py
@@ -22,7 +22,8 @@ class RnaDegradationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
         model = self.model
         cytosol = model.compartments.get_or_create(id='c')
         cytosol.name = 'cytosol'
-        cytosol.initial_volume = cell.properties.get_one(id='mean_volume').value
+        cytosol.initial_volume = cell.properties.get_one(
+            id='mean_volume').value
 
     def gen_species(self):
         """ Generate species associated with submodel """
@@ -41,37 +42,54 @@ class RnaDegradationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
                 species_type.empirical_formula = rna.get_empirical_formula()
                 species_type.molecular_weight = rna.get_mol_wt()
                 species_type.charge = rna.get_charge()
-                species = species_type.species.get_or_create(compartment=cytosol)
-                species.concentration = wc_lang.Concentration(value=rna.concentration, units=wc_lang.ConcentrationUnit.M)
+                species = species_type.species.get_or_create(
+                    compartment=cytosol)
+                species.concentration = wc_lang.Concentration(
+                    value=rna.concentration, units=wc_lang.ConcentrationUnit.M)
 
     def gen_reactions(self):
         """ Generate reactions associated with submodel """
         model = self.model
         submodel = self.submodel
         cytosol = model.compartments.get_one(id='c')
-        amp = model.species_types.get_one(id='amp').species.get_one(compartment=cytosol)
-        cmp = model.species_types.get_one(id='cmp').species.get_one(compartment=cytosol)
-        gmp = model.species_types.get_one(id='gmp').species.get_one(compartment=cytosol)
-        ump = model.species_types.get_one(id='ump').species.get_one(compartment=cytosol)
-        h2o = model.species_types.get_one(id='h2o').species.get_one(compartment=cytosol)
-        h = model.species_types.get_one(id='h').species.get_one(compartment=cytosol)
+        amp = model.species_types.get_one(
+            id='amp').species.get_one(compartment=cytosol)
+        cmp = model.species_types.get_one(
+            id='cmp').species.get_one(compartment=cytosol)
+        gmp = model.species_types.get_one(
+            id='gmp').species.get_one(compartment=cytosol)
+        ump = model.species_types.get_one(
+            id='ump').species.get_one(compartment=cytosol)
+        h2o = model.species_types.get_one(
+            id='h2o').species.get_one(compartment=cytosol)
+        h = model.species_types.get_one(
+            id='h').species.get_one(compartment=cytosol)
 
         cell = self.knowledge_base.cell
         kb_rnas = cell.species_types.get(__type=wc_kb.RnaSpeciesType)
         for kb_rna in kb_rnas:
-            rxn = submodel.reactions.get_or_create(id=kb_rna.id.replace('rna_', 'rna_degradation_'))
+            rxn = submodel.reactions.get_or_create(
+                id=kb_rna.id.replace('rna_', 'rna_degradation_'))
             rxn.name = kb_rna.name.replace('RNA ', 'RNA degradation')
 
-            model_rna = model.species_types.get_one(id=kb_rna.id).species.get_one(compartment=cytosol)
+            model_rna = model.species_types.get_one(
+                id=kb_rna.id).species.get_one(compartment=cytosol)
             seq = kb_rna.get_seq()
             rxn.participants = []
-            rxn.participants.add(model_rna.species_coefficients.get_or_create(coefficient=-1))
-            rxn.participants.add(h2o.species_coefficients.get_or_create(coefficient=-(kb_rna.get_len() - 1)))
-            rxn.participants.add(amp.species_coefficients.get_or_create(coefficient=seq.count('A')))
-            rxn.participants.add(cmp.species_coefficients.get_or_create(coefficient=seq.count('C')))
-            rxn.participants.add(gmp.species_coefficients.get_or_create(coefficient=seq.count('G')))
-            rxn.participants.add(ump.species_coefficients.get_or_create(coefficient=seq.count('U')))
-            rxn.participants.add(h.species_coefficients.get_or_create(coefficient=kb_rna.get_len() - 1))
+            rxn.participants.add(
+                model_rna.species_coefficients.get_or_create(coefficient=-1))
+            rxn.participants.add(h2o.species_coefficients.get_or_create(
+                coefficient=-(kb_rna.get_len() - 1)))
+            rxn.participants.add(amp.species_coefficients.get_or_create(
+                coefficient=seq.count('A')))
+            rxn.participants.add(cmp.species_coefficients.get_or_create(
+                coefficient=seq.count('C')))
+            rxn.participants.add(gmp.species_coefficients.get_or_create(
+                coefficient=seq.count('G')))
+            rxn.participants.add(ump.species_coefficients.get_or_create(
+                coefficient=seq.count('U')))
+            rxn.participants.add(h.species_coefficients.get_or_create(
+                coefficient=kb_rna.get_len() - 1))
 
     def gen_rate_laws(self):
         """ Generate rate laws associated with submodel """
@@ -83,7 +101,8 @@ class RnaDegradationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
         for rna, rxn in zip(rnas, self.submodel.reactions):
             rl = rxn.rate_laws.create()
             rl.direction = wc_lang.RateLawDirection.forward
-            rl.equation = wc_lang.RateLawEquation(expression='k_cat * {0}[c] / (k_m + {0}[c])'.format(rna.id))
+            rl.equation = wc_lang.RateLawEquation(
+                expression='k_cat * {0}[c] / (k_m + {0}[c])'.format(rna.id))
             rl.k_cat = 2 * numpy.log(2) / rna.half_life
             rl.k_m = rna.concentration
             rl.equation.modifiers.append(rxn.participants[0].species)
