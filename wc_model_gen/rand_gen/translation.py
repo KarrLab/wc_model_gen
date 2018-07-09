3f7960bd883e2d862b83a258bf752c9f54271f41
Ashwin-Srinivasan <ashwins9999@gmail.com>
2018-07-09 16:02:17 -0400

    Debugged TranslationSubmodelGen so that rand_wc_model_gen works

--- wc_model_gen/rand_gen/translation.py
+++ wc_model_gen/rand_gen/translation.py
@@ -17,19 +17,21 @@ class TranslationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
 
     def gen_species(self):
         """ Generate a set of 2 protein species (_att: attached to ribosome and acitve form) for each protein """
+
+        #print('here')
         submodel = self.submodel
         compartment = self.model.compartments.get_one(id='c')
 
         # Initiate ribosome species types (complexes)
-        species_type = self.model.species_types.create(id='complex_70S_IA', name='complex_70S_IA', type=5)
-        species_type.molecular_weight = '1' #placeholder
+        species_type = self.model.species_types.create(id='complex_70S_IA', name='complex_70S_IA', type=wc_lang.SpeciesTypeType.pseudo_species)
+        species_type.molecular_weight = 1 #placeholder
         species = species_type.species.create(compartment=compartment)
-        species.concentration = wc_lang.core.Concentration(value=1e-2, units='M')
+        species.concentration = wc_lang.core.Concentration(value=1e-2, units=wc_lang.ConcentrationUnit.M)
 
-        species_type = self.model.species_types.create(id='complex_70S_A', name='complex_70S_A', type=5)
-        species_type.molecular_weight = '1' #placeholder
+        species_type = self.model.species_types.create(id='complex_70S_A', name='complex_70S_A', type=wc_lang.SpeciesTypeType.pseudo_species)
+        species_type.molecular_weight = 1 #placeholder
         species = species_type.species.create(compartment=compartment)
-        species.concentration = wc_lang.core.Concentration(value=1e-2, units='M')
+        species.concentration = wc_lang.core.Concentration(value=1e-2, units=wc_lang.ConcentrationUnit.M)
 
         # Create both functional and afunctional form (_att: attached to RNA) of every protein in KB
         for protein in self.knowledge_base.cell.species_types.get(__type=wc_kb.core.ProteinSpeciesType):
@@ -37,67 +39,72 @@ class TranslationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
             # Add functional form of protein
             species_type = self.model.species_types.create(
                 id=protein.id,
-                type=2,
+                type=wc_lang.SpeciesTypeType.protein,
                 name=protein.name,
                 structure=protein.get_seq(),
-                empirical_formula=str(protein.get_empirical_formula()),
-                molecular_weight=str(protein.get_mol_wt()),
-                charge=str(protein.get_charge()))
+                empirical_formula=protein.get_empirical_formula(),
+                molecular_weight=protein.get_mol_wt(),
+                charge=protein.get_charge())
 
             species = species_type.species.create(compartment=compartment)
-            species.concentration = wc_lang.core.Concentration(value=0, units='M')
+            species.concentration = wc_lang.core.Concentration(value=0, units=wc_lang.ConcentrationUnit.M)
 
             # Add inactive form of protein, attached to ribosome
             species_type = self.model.species_types.create(
                 id=protein.id+'_att',
-                type=2,
+                type=wc_lang.SpeciesTypeType.protein,
                 name=protein.name+'_att',
                 structure=protein.get_seq(),
-                empirical_formula=str(protein.get_empirical_formula()),
-                molecular_weight=str(protein.get_mol_wt()),
-                charge=str(protein.get_charge()))
+                empirical_formula=protein.get_empirical_formula(),
+                molecular_weight=protein.get_mol_wt(),
+                charge=protein.get_charge())
 
             species = species_type.species.create(compartment=compartment)
-            species.concentration = wc_lang.core.Concentration(value=0, units='M')
+            species.concentration = wc_lang.core.Concentration(value=0, units=wc_lang.ConcentrationUnit.M)
 
     def gen_reactions(self):
         """ Generate a set of 3 reqactions (initation, elongation, termination) for each protein """
-
+        #print('here')
         submodel = self.submodel
         compartment = self.model.compartments.get_one(id='c')
-        prots = self.knowledge_base.cell.species_types.get(__type=wc_kb.core.ProteinSpeciesType)
-        rnas = self.knowledge_base.cell.species_types.get(__type=wc_kb.core.RnaSpeciesType)
+        prots = self.knowledge_base.cell.species_types.get(__type=wc_kb.ProteinSpeciesType)
+        rnas = self.knowledge_base.cell.species_types.get(__type=wc_kb.RnaSpeciesType)
+
+        print(len(rnas))
         trnas = []
         for rna in rnas:
             if rna.type == wc_kb.RnaType.tRna:
                 trnas.append(rna)
-        protsList = []
+        #print(len(trnas))
+        protsSet = set()
         IF1 = random.choice(prots).id
-        protsList.append(IF1)
+        protsSet.add(IF1)
+        #print('here1')
         IF2 = random.choice(prots).id
-        while IF2 in protsList:
+        while IF2 in protsSet:
+            #print('here2')
             IF2 = random.choice(prots).id
-        protsList.append(IF2)
+        protsSet.add(IF2)
         IF3 = random.choice(prots).id
-        while IF3 in protsList:
+        while IF3 in protsSet:
             IF3 = random.choice(prots).id
-        protsList.append(IF3)
+        protsSet.add(IF3)
         EFtu = random.choice(prots).id
-        while EFtu in protsList:
+        while EFtu in protsSet:
             EFtu = random.choice(prots).id
-        protsList.append(EFtu)
+        protsSet.add(EFtu)
         EFts = random.choice(prots).id
-        while EFts in protsList:
+        while EFts in protsSet:
             EFts = random.choice(prots).id
-        protsList.append(EFts)
+        protsSet.add(EFts)
         EFg = random.choice(prots).id
-        while EFg in protsList:
+        while EFg in protsSet:
             EFg = random.choice(prots).id
-        protsList.append(EFg)
+        protsSet.add(EFg)
         RF = random.choice(prots).id
-        while RF in protsList:
-            IF2 = random.choice(prots).id
-        protsList.append(IF2)
+        while RF in protsSet:
+            RF = random.choice(prots).id
+        protsSet.add(RF)
         amino_acids = {}
         trna = random.choice(trnas)
         amino_acids['S'] = ['tRNA-Ser', trna.id]
@@ -215,7 +222,7 @@ class TranslationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
 
                 n = protein.get_seq().tostring().count(letter)
                 specie = self.model.species_types.get_or_create(
-                    id=amino_acids[letter][1], type=4).species.get_or_create(compartment=compartment)
+                    id=amino_acids[letter][1], type=wc_lang.SpeciesTypeType.rna).species.get_or_create(compartment=compartment)
                 reaction.participants.add(specie.species_coefficients.get_or_create(coefficient=-n))
 
             # Adding reaction participants RHS
