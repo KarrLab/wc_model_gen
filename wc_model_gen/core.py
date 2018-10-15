757974ff0da8afe738f30af5c0201abe62ad6458
balazs1987 <szbazsu@gmail.com>
2018-10-15 12:03:35 -0400

    pychace try

--- wc_model_gen/core.py
+++ wc_model_gen/core.py
@@ -14,7 +14,11 @@ import wc_lang
 import math
 import numpy
 import wc_utils.util.string
-
+from wc_sim.multialgorithm.simulation import Simulation
+from wc_sim.multialgorithm.run_results import RunResults
+from wc_lang.util import get_model_summary
+import time
+import os
 
 class ModelGenerator(object):
     """ Generator for models (:obj:`wc_lang.Model`)
@@ -81,6 +85,82 @@ class ModelGenerator(object):
             component_generator(self.knowledge_base, model, options=options).run()
 
         return model
+
+    """ Not sure what is the best place for the following static methods """
+    @staticmethod
+    def gen_rand_min_model_kb(name=None):
+        """ Generates a random min model KB """
+
+        kb = wc_kb_gen.random.RandomKbGenerator(options={
+                     'component': {
+                         'GenomeGenerator': {
+                             'genetic_code': 'reduced',
+                             'num_genes': 16,
+                             'mean_gene_len': 50,
+                             'num_tRNA': 4,
+                             'num_rRNA': 0,
+                             'num_ncRNA': 0,
+                             'min_prots': 5,
+                             'translation_table': 4,
+                             'mean_rna_copy_number': 100,
+                             'mean_protein_copy_number': 100},
+                         'PropertiesGenerator': {'cell_cycle_length': 100},
+                         'ObservablesGenerator': {'genetic_code': 'reduced'}}}).run()
+
+        cytosol = kb.cell.compartments.get_one(id='c')
+        kb.cell.species_types.get_one(id='amp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='cmp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='gmp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='ump').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='atp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='ctp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='gtp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+        kb.cell.species_types.get_one(id='utp').species.get_one(compartment=cytosol).concentrations.value='0.000008333'
+
+        if name:
+            core_path = '/media/sf_VM_share/kbs/' + str(name) +'.xlsx'
+            seq_path  = '/media/sf_VM_share/kbs/' + str(name) +'_seq.fna'
+
+            wc_kb.io.Writer().run(knowledge_base=kb,
+                                  core_path=core_path ,
+                                  seq_path=seq_path,
+                                  set_repo_metadata_from_path=False)
+
+        return kb
+
+    @staticmethod
+    def run_model(model, results_dir, checkpoint_period=5, end_time=100):
+        """ Simulates model """
+
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+
+        simulation = Simulation(model)
+        results = simulation.run(end_time, results_dir, checkpoint_period)
+
+        return results
+
+    @staticmethod
+    def analyze_model(self, results):
+        """ Prints the standard analysis of simulation results """
+
+        num_events  = results[0]
+        run_results_dir = results[1]
+        run_results = RunResults(run_results_dir)
+
+        rna_ids=[]
+        df = run_results.get('populations')
+        for rna in model.species_types.get(type = wc_lang.SpeciesTypeType.rna):
+            rna_ids.append(rna.species[0].id())
+
+        txt = 'Init copy number mean={}; std={} \n'.format(round(np.mean(df.loc[0.0,rna_ids].values),2),
+                                                            round(np.std(df.loc[0.0,rna_ids].values),2))
+        txt += 'Final copy number mean={}; std={}'.format(round(np.mean(df.loc[100.0,rna_ids].values),2),
+                                                          round(np.std(df.loc[100.0,rna_ids].values),2))
+        print(txt)
+        print(df['h[c]'],'\n')
+        print(df[['amp[c]','cmp[c]','gmp[c]','ump[c]']],'\n')
+        print(df[['atp[c]','ctp[c]','gtp[c]','utp[c]']],'\n')
 
 class ModelComponentGenerator(six.with_metaclass(abc.ABCMeta, object)):
     """ Abstract base class for model component generators
