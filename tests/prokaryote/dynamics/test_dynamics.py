a5fdafc82c9cd02e40ec4d69b34f8f8dfd492aa5
YinHoon <yinhoon.chew@mssm.edu>
2019-02-21 13:00:22 -0500

    Clean up tests in accordance with latest changes

--- tests/prokaryote/dynamics/test_dynamics.py
+++ tests/prokaryote/dynamics/test_dynamics.py
@@ -21,7 +21,7 @@ import wc_kb_gen
 import wc_lang
 
 
-class PhenomenologicalTestCase(unittest.TestCase):
+class DynamicsTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -49,69 +49,9 @@ class PhenomenologicalTestCase(unittest.TestCase):
                                   prokaryote.MetabolismSubmodelGenerator],
             options={'component': {
                 'TranscriptionSubmodelGenerator': {
-                    'rate_dynamics': 'phenomenological'},
+                    'beta': 1.},
                 'RnaDegradationSubmodelGenerator': {
-                    'rate_dynamics': 'phenomenological'}}}).run()
-
-        # Simulate model
-        checkpoint_period = 10
-        end_time = 100
-        simulation = Simulation(self.model)
-        results = simulation.run(end_time, self.dir, checkpoint_period)
-        self.assertIsInstance(results, tuple)
-
-        # Check results
-        num_events = results[0]
-        run_results_dir = results[1]
-        run_results = RunResults(run_results_dir)
-        df = run_results.get('populations')
-
-        cytosol = self.model.compartments.get_one(id='c')
-        rna_ids = []
-        for rna in self.model.species_types.get(type=wcm_ontology['WCM:RNA']): # RNA
-            rna_ids.append(rna.species.get_one(compartment=cytosol).id)
-
-        avg_init_rna_cn = np.mean(df.loc[0.0, rna_ids].values)
-        avg_final_rna_cn = np.mean(df.loc[100.0, rna_ids].values)
-
-        print(simulation.provide_event_counts())
-        print('\n INIT AVG RNA COPY NUMBERS:', avg_init_rna_cn)
-        print('\n FINAL AVG RNA COPY NUMBERS:', avg_final_rna_cn)
-
-        self.assertTrue(abs(2*avg_init_rna_cn-avg_final_rna_cn) < avg_init_rna_cn*0.15)
-
-
-class MechanisticDynamicsTestCase(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.dir = tempfile.mkdtemp()
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.dir)
-
-    def test_growth_transcription(self):
-
-        # Construct model
-        env = EnvironmentVarGuard()
-        env.set('CONFIG__DOT__wc_kb__DOT__io__DOT__strict', '0')
-        with env:
-            self.kb = wc_kb.io.Reader().run('tests/fixtures/min_model_kb.xlsx',
-                                            'tests/fixtures/min_model_kb_seq.fna',
-                                            )[wc_kb.KnowledgeBase][0]
-
-        self.model = prokaryote.ProkaryoteModelGenerator(
-            knowledge_base=self.kb,
-            component_generators=[prokaryote.InitalizeModel,
-                                  prokaryote.TranscriptionSubmodelGenerator,
-                                  prokaryote.RnaDegradationSubmodelGenerator,
-                                  prokaryote.MetabolismSubmodelGenerator],
-            options={'component': {
-                'TranscriptionSubmodelGenerator': {
-                    'rate_dynamics': 'mechanistic'},
-                'RnaDegradationSubmodelGenerator': {
-                    'rate_dynamics': 'mechanistic'}}}).run()
+                    'beta': 1.}}}).run()
 
         # Simulate model
         checkpoint_period = 10
