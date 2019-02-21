Fix rate_law type and units; Minor documentation edits

tests/test_utils.py
@@ -25,6 +25,8 @@ class TestCase(unittest.TestCase):
     def test_MM_like_rate_law(self):
 
         Avogadro = wc_lang.Parameter(id='Avogadro', value=scipy.constants.Avogadro)
+        molecule_units = wc_lang.Parameter(id='molecule_units', value=1.,
+            units=unit_registry.parse_units('molecule'))
 		
         c = wc_lang.Compartment(id='c', mean_init_volume=0.5)
         c.init_density = wc_lang.Parameter(id='density_' + c.id, value=1.)                
@@ -66,9 +68,9 @@ class TestCase(unittest.TestCase):
             participant4, participant5, participant6, participant7, participant8])
 
         rate_law, parameters = utils.MM_like_rate_law(
-            Avogadro, reaction, modifiers=[modifier1, modifier2], modifier_reactants=[species['s6_c']])
+            Avogadro, molecule_units, reaction, modifiers=[modifier1, modifier2], modifier_reactants=[species['s6_c']])
 
-        self.assertEqual(rate_law.expression, 'k_cat_r1 * e1 * e2 * '
+        self.assertEqual(rate_law.expression, 'k_cat_r1 / molecule_units * e1 * e2 * '
             '(s1[c] / (s1[c] + K_m_r1_s1 * Avogadro * volume_c)) * '
             '(s2[c] / (s2[c] + K_m_r1_s2 * Avogadro * volume_c)) * '
             '(s6[c] / (s6[c] + K_m_r1_s6 * Avogadro * volume_c))')

wc_model_gen/prokaryote/rna_degradation.py
@@ -20,7 +20,19 @@ import math
 
 
 class RnaDegradationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
-    """ Generator for RNA degradation submodel """
+    """ Generator for RNA degradation submodel 
+
+        Options:
+        * beta (:obj:`float`, optional): ratio of Michaelis-Menten constant to substrate 
+            concentration (Km/[S]) for use when estimating Km values, the default value is 1
+    """
+
+    def clean_and_validate_options(self):
+        """ Apply default options and validate options """
+        options = self.options
+
+        beta = options.get('beta', 1.)
+        options['beta'] = beta
 
     def gen_reactions(self):
         """ Generate reactions associated with submodel """
@@ -71,44 +83,53 @@ class RnaDegradationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
     def gen_rate_laws(self):
         """ Generate rate laws for the reactions in the submodel """
         beta = self.options.get('beta')
-        Avogadro = self.model.parameters.get_or_create(id='Avogadro',
-                                                type=None,
-                                                value=scipy.constants.Avogadro,
-                                                units=unit_registry.parse_units('molecule mol^-1'))
+        Avogadro = self.model.parameters.get_or_create(
+            id='Avogadro',
+            type=None,
+            value=scipy.constants.Avogadro,
+            units=unit_registry.parse_units('molecule mol^-1'))
+        molecule_units = self.model.parameters.get_or_create(
+            id='molecule_units',
+            type=None,
+            value=1.,
+            units=unit_registry.parse_units('molecule'))
 
         modifier = self.model.observables.get_one(id='degrade_rnase_obs')
 
         for reaction in self.submodel.reactions:
             
-            rate_law_exp, parameters = utils.MM_like_rate_law(Avogadro, reaction, beta=beta, modifiers=[modifier])
+            rate_law_exp, parameters = utils.MM_like_rate_law(
+                Avogadro, molecule_units, reaction, beta=beta, modifiers=[modifier])
             self.model.parameters += parameters
 
             rate_law = wc_lang.RateLaw(direction=wc_lang.RateLawDirection.forward,
-                                        type=wcm_ontology['WCM:rate_law'],
+                                        type=None,
                                         expression=rate_law_exp,
-                                        units=unit_registry.parse_units('molecule s^-1'))
-            reaction.rate_laws.append(rate_law)
+                                        reaction=reaction,
+                                        )
+            rate_law.id = rate_law.gen_id()
+            
 
     def calibrate_submodel(self):
         """ Calibrate the submodel using data in the KB """
         
         cytosol = self.model.compartments.get_one(id='c')
-                
+        
         init_species_counts = {}
         
         modifier = self.model.observables.get_one(id='degrade_rnase_obs')        
         for species in modifier.expression.species:
-            init_species_counts[species.gen_id()] = species.distribution_init_concentration.mean             
+            init_species_counts[species.gen_id()] = species.distribution_init_concentration.mean
         
         rnas_kb = self.knowledge_base.cell.species_types.get(__type=wc_kb.prokaryote_schema.RnaSpeciesType)
         for rna_kb, reaction in zip(rnas_kb, self.submodel.reactions):
             
-            rna_reactant = self.model.species_types.get_one(id=rna_kb.id).species.get_one(compartment=cytosol)
+            rna_reactant = self.model.species_types.get_one(id=rna_kb.id).species.get_one(compartment=cytosol)            
             half_life = rna_kb.half_life
             mean_concentration = rna_reactant.distribution_init_concentration.mean         
 
             average_rate = math.log(2) / half_life * mean_concentration
-            
+                        
             for species in reaction.get_reactants():
                 init_species_counts[species.gen_id()] = species.distribution_init_concentration.mean
 

wc_model_gen/prokaryote/translation.py
@@ -19,7 +19,19 @@ import wc_kb
 
 
 class TranslationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
-    """ Generate translation submodel. """
+    """ Generate translation submodel 
+
+        Options:
+        * beta (:obj:`float`, optional): ratio of Michaelis-Menten constant to substrate 
+            concentration (Km/[S]) for use when estimating Km values, the default value is 1
+    """
+
+    def clean_and_validate_options(self):
+        """ Apply default options and validate options """
+        options = self.options
+
+        beta = options.get('beta', 1.)
+        options['beta'] = beta
 
     def gen_reactions(self):
         """ Generate a lumped reaction that covers initiation, elongation and termination for each protein translated """
@@ -135,21 +147,29 @@ class TranslationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
     def gen_rate_laws(self):
         """ Generate rate laws for the reactions in the submodel """
         beta = self.options.get('beta')
-        Avogadro = self.model.parameters.get_or_create(id='Avogadro',
-                                                type=None,
-                                                value=scipy.constants.Avogadro,
-                                                units=unit_registry.parse_units('molecule mol^-1'))
+        Avogadro = self.model.parameters.get_or_create(
+            id='Avogadro',
+            type=None,
+            value=scipy.constants.Avogadro,
+            units=unit_registry.parse_units('molecule mol^-1'))
+        molecule_units = self.model.parameters.get_or_create(
+            id='molecule_units',
+            type=None,
+            value=1.,
+            units=unit_registry.parse_units('molecule'))
 
         for reaction in self.submodel.reactions:        
-            rate_law_exp, parameters = utils.MM_like_rate_law(Avogadro, reaction, beta=beta, modifiers=self._modifiers)
+            rate_law_exp, parameters = utils.MM_like_rate_law(
+                Avogadro, molecule_units, reaction, beta=beta, modifiers=self._modifiers)
             self.model.parameters += parameters
 
             rate_law = wc_lang.RateLaw(direction=wc_lang.RateLawDirection.forward,
-                                        type=wcm_ontology['WCM:rate_law'],
+                                        type=None,
                                         expression=rate_law_exp,
-                                        units=unit_registry.parse_units('molecule s^-1'))
-            reaction.rate_laws.append(rate_law)
-    
+                                        reaction=reaction,
+                                        )
+            rate_law.id = rate_law.gen_id()
+                
     def calibrate_submodel(self):
         """ Calibrate the submodel using data in the KB """
 