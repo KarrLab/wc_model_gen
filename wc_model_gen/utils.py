ae382021360d795076142c2494f05021bfd9af65
paulflang <paul.lang@wolfson.ox.ac.uk>
2019-03-13 21:54:27 -0400

    tried to implement a mass_action_rate_law

--- wc_model_gen/utils.py
+++ wc_model_gen/utils.py
@@ -126,3 +126,76 @@ def MM_like_rate_law(Avogadro, molecule_units, reaction, modifiers=None, modifie
     assert error is None, str(error)
 
     return rate_law_expression, list(parameters.values()) 	
+
+def mass_action_rate_law(Avogadro, molecule_units, reaction, kinetic_parameter_value, modifiers=None, modifier_reactants=None):
+    """ Generate a mass action rate law.
+
+        Example:
+
+            Rate = k * [E1] * [S1]
+
+            where
+                k_: rate constant (e.g.: association, dissociation or catalytic constant)
+                [En]: concentration of nth enzyme (modifier)
+                [Sn]: concentration of nth substrate  
+
+        Args:
+            Avogadro (:obj:`wc_lang.Parameter`): model parameter for Avogadro number
+            molecule_units (:obj:`wc_lang.Parameter`): model parameter for molecule units
+            reaction (:obj:`wc_lang.Reaction`): reaction    
+            modifiers (:obj:`list` of :obj:`wc_lang.Observable`): list of observables,
+                each of which evaluates to the total concentration of all enzymes that 
+                catalyze the same intermediate step in the reaction
+            modifier_reactants (:obj:`list` of :obj:`wc_lang.Species`): list of species 
+                in modifiers that should be included as reactants in the rate law    
+            
+        Returns:
+            :obj:`wc_lang.RateLawExpression`: rate law
+            :obj:`list` of :obj:`wc_lang.Parameter`: list of parameters in the rate law     
+    """
+    if modifiers is None:
+        modifier_species = []
+    else:
+        modifier_species = [i for modifier in modifiers for i in modifier.expression.species]
+
+    if modifier_reactants is None:
+        additional_reactants = []
+    else:
+        additional_reactants = modifier_reactants
+
+    parameters = {}
+    parameters[Avogadro.id] = Avogadro
+    parameters[molecule_units.id] = molecule_units            
+
+    model_k = wc_lang.Parameter(id='k_{}'.format(reaction.id),
+                                    type=None,
+                                    units=unit_registry.parse_units('s^-1'),
+                                    value= kinetic_parameter_value),
+    parameters[model_k.id] = model_k
+
+    expression_terms = []
+    all_species = {}
+    for species in reaction.get_reactants():
+                
+        if species not in modifier_species or species in additional_reactants:
+        
+            all_species[species.gen_id()] = species
+            volume = species.compartment.init_density.function_expressions[0].function
+            
+            expression_terms.append(str(species.gen_id())) 
+
+    expression = '{} / {} * {} * {}'.format(
+        model_k_cat.id,
+        molecule_units.id, 
+        ' * '.join([i.id for i in modifiers]), 
+        ' * '.join(expression_terms))
+    
+    rate_law_expression, error = wc_lang.RateLawExpression.deserialize(expression, {
+        wc_lang.Parameter: parameters,
+        wc_lang.Species: all_species,
+        wc_lang.Observable: {i.id: i for i in modifiers},
+        wc_lang.Function: {volume.id: volume},
+        })
+    assert error is None, str(error)
+
+    return rate_law_expression, list(parameters.values())   
