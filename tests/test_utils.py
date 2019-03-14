correcting units, refactoring rate law calculations, other debugging

tests/test_utils.py
@@ -17,22 +17,18 @@ import wc_lang
 
 class TestCase(unittest.TestCase):
 
-    def test_calculate_average_synthesis_rate(self):
+    def test_calc_avg_syn_rate(self):
 
         test_rate = utils.calc_avg_syn_rate(0.5, 300., 36000.)
         self.assertAlmostEqual(test_rate, 0.001164872, places=9)
 
-    def test_calculate_average_degradation_rate(self):
+    def test_calc_avg_deg_rate(self):
 
         test_rate = utils.calc_avg_deg_rate(0.5, 300.)
         self.assertAlmostEqual(test_rate, 0.0011552453009332421, places=16)    
 
     def test_gen_michaelis_menten_like_rate_law(self):
-
-        Avogadro = wc_lang.Parameter(id='Avogadro', value=scipy.constants.Avogadro)
-        molecule_units = wc_lang.Parameter(id='molecule_units', value=1.,
-            units=unit_registry.parse_units('molecule'))
-		
+        model = wc_lang.Model()
         c = wc_lang.Compartment(id='c', mean_init_volume=0.5)
         c.init_density = wc_lang.Parameter(id='density_' + c.id, value=1.)                
         volume = wc_lang.Function(id='volume_' + c.id)
@@ -73,9 +69,9 @@ class TestCase(unittest.TestCase):
             participant4, participant5, participant6, participant7, participant8])
 
         rate_law, parameters = utils.gen_michaelis_menten_like_rate_law(
-            Avogadro, molecule_units, reaction, modifiers=[modifier1, modifier2], modifier_reactants=[species['s6_c']])
+            model, reaction, modifiers=[modifier1, modifier2], modifier_reactants=[species['s6_c']])
 
-        self.assertEqual(rate_law.expression, 'k_cat_r1 / molecule_units * e1 * e2 * '
+        self.assertEqual(rate_law.expression, 'k_cat_r1 * e1 * e2 * '
             '(s1[c] / (s1[c] + K_m_r1_s1 * Avogadro * volume_c)) * '
             '(s2[c] / (s2[c] + K_m_r1_s2 * Avogadro * volume_c)) * '
             '(s6[c] / (s6[c] + K_m_r1_s6 * Avogadro * volume_c))')
@@ -83,6 +79,6 @@ class TestCase(unittest.TestCase):
         self.assertEqual(set(rate_law.observables), set([modifier1, modifier2]))
         self.assertEqual(set(rate_law.parameters), set(parameters))        
         self.assertEqual(rate_law.parameters.get_one(id='k_cat_r1').type, wcm_ontology['WCM:k_cat'])
-        self.assertEqual(rate_law.parameters.get_one(id='k_cat_r1').units, unit_registry.parse_units('s^-1'))
+        self.assertEqual(rate_law.parameters.get_one(id='k_cat_r1').units, unit_registry.parse_units('s^-1 molecule^-2'))
         self.assertEqual(rate_law.parameters.get_one(id='K_m_r1_s2').type, wcm_ontology['WCM:K_m'])
         self.assertEqual(rate_law.parameters.get_one(id='K_m_r1_s2').units, unit_registry.parse_units('M'))

wc_model_gen/prokaryote/protein_degradation.py
@@ -104,28 +104,17 @@ class ProteinDegradationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
 
         model = self.model
 
-        Avogadro = model.parameters.get_or_create(
-            id='Avogadro',
-            type=None,
-            value=scipy.constants.Avogadro,
-            units=unit_registry.parse_units('molecule mol^-1'))
-        molecule_units = model.parameters.get_or_create(
-            id='molecule_units',
-            type=None,
-            value=1.,
-            units=unit_registry.parse_units('molecule'))
-
         modifier = model.observables.get_one(id='degrade_protease_obs')
 
         for reaction in self.submodel.reactions:
             modifier_reactant = [i for i in modifier.expression.species if i.species_type.id in reaction.id]
             if modifier_reactant:
                 rate_law_exp, parameters = utils.gen_michaelis_menten_like_rate_law(
-                    Avogadro, molecule_units, reaction, modifiers=[modifier],
+                    model, reaction, modifiers=[modifier],
                     modifier_reactants=modifier_reactant)
             else:
                 rate_law_exp, parameters = utils.gen_michaelis_menten_like_rate_law(
-                    Avogadro, molecule_units, reaction, modifiers=[modifier])
+                    model, reaction, modifiers=[modifier])
 
             model.parameters += parameters
 

wc_model_gen/prokaryote/translation.py
@@ -148,20 +148,9 @@ class TranslationSubmodelGenerator(wc_model_gen.SubmodelGenerator):
         """ Generate rate laws for the reactions in the submodel """
         model = self.model
 
-        Avogadro = model.parameters.get_or_create(
-            id='Avogadro',
-            type=None,
-            value=scipy.constants.Avogadro,
-            units=unit_registry.parse_units('molecule mol^-1'))
-        molecule_units = model.parameters.get_or_create(
-            id='molecule_units',
-            type=None,
-            value=1.,
-            units=unit_registry.parse_units('molecule'))
-
         for reaction in self.submodel.reactions:
             rate_law_exp, parameters = utils.gen_michaelis_menten_like_rate_law(
-                Avogadro, molecule_units, reaction, modifiers=self._modifiers)
+                model, reaction, modifiers=self._modifiers)
             model.parameters += parameters
 
             rate_law = model.rate_laws.create(direction=wc_lang.RateLawDirection.forward,

wc_model_gen/utils.py
@@ -9,23 +9,25 @@
 from wc_utils.util.ontology import wcm_ontology
 from wc_utils.util.units import unit_registry
 import math
+import scipy.constants
 import wc_lang
 
 
 def calc_avg_syn_rate(mean_concentration, half_life, mean_doubling_time):
-	""" Calculate the average synthesis rate of a species over a cell cycle
+    """ Calculate the average synthesis rate of a species over a cell cycle
 
-	    Args:
-	    	mean_concentration (:obj:`float`): species mean concentration
-	    	half_life (:obj:`float`): species half life
-	    	mean_doubling_time (:obj:`float`): mean doubling time of cells
+        Args:
+            mean_concentration (:obj:`float`): species mean concentration
+            half_life (:obj:`float`): species half life
+            mean_doubling_time (:obj:`float`): mean doubling time of cells
 
-	    Returns:
-	    	:obj:`float`: the average synthesis rate of the species
-	"""
-	ave_synthesis_rate = math.log(2) * (1. / mean_doubling_time + 1. / half_life) * mean_concentration
+        Returns:
+            :obj:`float`: the average synthesis rate of the species
+    """
+    ave_synthesis_rate = math.log(2) * (1. / mean_doubling_time + 1. / half_life) * mean_concentration
 
-	return ave_synthesis_rate
+    return ave_synthesis_rate
+
 
 def calc_avg_deg_rate(mean_concentration, half_life):
     """ Calculate the average degradation rate of a species over a cell cycle
@@ -33,15 +35,16 @@ def calc_avg_deg_rate(mean_concentration, half_life):
         Args:
             mean_concentration (:obj:`float`): species mean concentration
             half_life (:obj:`float`): species half life
-            
+
         Returns:
             :obj:`float`: the average degradation rate of the species
     """
     ave_degradation_rate = math.log(2) / half_life * mean_concentration
 
-    return ave_degradation_rate    
+    return ave_degradation_rate
 
-def gen_michaelis_menten_like_rate_law(avogadro, molecule_units, reaction, modifiers=None, modifier_reactants=None):
+
+def gen_michaelis_menten_like_rate_law(model, reaction, modifiers=None, modifier_reactants=None):
     """ Generate a Michaelis-Menten-like rate law. For a multi-substrate reaction,  
         the substrate term is formulated as the multiplication of a Hill equation
         with a coefficient of 1 for each substrate. For multi-steps reaction where
@@ -50,27 +53,26 @@ def gen_michaelis_menten_like_rate_law(avogadro, molecule_units, reaction, modif
 
         Example:
 
-        	Rate = k_cat * [E1] * [E2] * [S1]/(Km_S1 + [S1]) * [S2]/(Km_S2 + [S2])
+                Rate = k_cat * [E1] * [E2] * [S1]/(Km_S1 + [S1]) * [S2]/(Km_S2 + [S2])
 
-        	where
-        	    k_cat: catalytic constant
-            	[En]: concentration of nth enzyme (modifier)
-            	[Sn]: concentration of nth substrate
-            	Km_Sn: Michaelis-Menten constant for nth substrate   
+                where
+                    k_cat: catalytic constant
+                [En]: concentration of nth enzyme (modifier)
+                [Sn]: concentration of nth substrate
+                Km_Sn: Michaelis-Menten constant for nth substrate   
 
         Args:
-            avogadro (:obj:`wc_lang.Parameter`): model parameter for Avogadro number
-            molecule_units (:obj:`wc_lang.Parameter`): model parameter for molecule units
-        	reaction (:obj:`wc_lang.Reaction`): reaction    
-        	modifiers (:obj:`list` of :obj:`wc_lang.Observable`): list of observables,
+            model (:obj:`wc_lang.Model`): model
+                reaction (:obj:`wc_lang.Reaction`): reaction    
+                modifiers (:obj:`list` of :obj:`wc_lang.Observable`): list of observables,
                 each of which evaluates to the total concentration of all enzymes that 
                 catalyze the same intermediate step in the reaction
             modifier_reactants (:obj:`list` of :obj:`wc_lang.Species`): list of species 
                 in modifiers that should be included as reactants in the rate law    
-        	
+
         Returns:
-        	:obj:`wc_lang.RateLawExpression`: rate law
-        	:obj:`list` of :obj:`wc_lang.Parameter`: list of parameters in the rate law  	
+                :obj:`wc_lang.RateLawExpression`: rate law
+                :obj:`list` of :obj:`wc_lang.Parameter`: list of parameters in the rate law     
     """
     if modifiers is None:
         modifier_species = []
@@ -83,51 +85,56 @@ def gen_michaelis_menten_like_rate_law(avogadro, molecule_units, reaction, modif
         additional_reactants = modifier_reactants
 
     parameters = {}
-    parameters[avogadro.id] = avogadro
-    parameters[molecule_units.id] = molecule_units            
 
-    model_k_cat = wc_lang.Parameter(id='k_cat_{}'.format(reaction.id),
-                                    type=wcm_ontology['WCM:k_cat'],
-                                    units=unit_registry.parse_units('s^-1'))
-    parameters[model_k_cat.id] = model_k_cat    
+    avogadro = model.parameters.get_or_create(
+        id='Avogadro',
+        type=None,
+        value=scipy.constants.Avogadro,
+        units=unit_registry.parse_units('molecule mol^-1'))
+    parameters[avogadro.id] = avogadro
+
+    model_k_cat = model.parameters.get_or_create(id='k_cat_{}'.format(reaction.id),
+                                                 type=wcm_ontology['WCM:k_cat'],
+                                                 units=unit_registry.parse_units('s^-1 * molecule^{{-{}}}'.format(len(modifiers))))
+    parameters[model_k_cat.id] = model_k_cat
 
     expression_terms = []
     all_species = {}
     for species in reaction.get_reactants():
-                
+
         if species not in modifier_species or species in additional_reactants:
-        
+
             all_species[species.gen_id()] = species
-            
-            model_k_m = wc_lang.Parameter(id='K_m_{}_{}'.format(reaction.id, species.species_type.id),
-                type=wcm_ontology['WCM:K_m'],                
-                units=unit_registry.parse_units('M'))
+
+            model_k_m = model.parameters.create(id='K_m_{}_{}'.format(reaction.id, species.species_type.id),
+                                                type=wcm_ontology['WCM:K_m'],
+                                                units=unit_registry.parse_units('M'))
             parameters[model_k_m.id] = model_k_m
-            
+
             volume = species.compartment.init_density.function_expressions[0].function
-            
+
             expression_terms.append('({} / ({} + {} * {} * {}))'.format(species.gen_id(),
                                                                         species.gen_id(),
                                                                         model_k_m.id, avogadro.id,
-                                                                        volume.id))   
+                                                                        volume.id))
 
-    expression = '{} / {} * {} * {}'.format(
+    expression = '{} * {} * {}'.format(
         model_k_cat.id,
-        molecule_units.id, 
-        ' * '.join([i.id for i in modifiers]), 
+        ' * '.join([i.id for i in modifiers]),
         ' * '.join(expression_terms))
-    
+
     rate_law_expression, error = wc_lang.RateLawExpression.deserialize(expression, {
         wc_lang.Parameter: parameters,
         wc_lang.Species: all_species,
         wc_lang.Observable: {i.id: i for i in modifiers},
         wc_lang.Function: {volume.id: volume},
-        })
+    })
     assert error is None, str(error)
 
-    return rate_law_expression, list(parameters.values()) 	
+    return rate_law_expression, list(parameters.values())
 
-def gen_mass_action_rate_law(avogadro, molecule_units, reaction, kinetic_parameter_value, modifiers=None, modifier_reactants=None):
+
+def gen_mass_action_rate_law(model, reaction, kinetic_parameter_value, modifiers=None, modifier_reactants=None):
     """ Generate a mass action rate law.
 
         Example:
@@ -140,15 +147,14 @@ def gen_mass_action_rate_law(avogadro, molecule_units, reaction, kinetic_paramet
                 [Sn]: concentration of nth substrate  
 
         Args:
-            avogadro (:obj:`wc_lang.Parameter`): model parameter for Avogadro number
-            molecule_units (:obj:`wc_lang.Parameter`): model parameter for molecule units
+            model (:obj:`wc_lang.Model`): model
             reaction (:obj:`wc_lang.Reaction`): reaction    
             modifiers (:obj:`list` of :obj:`wc_lang.Observable`): list of observables,
                 each of which evaluates to the total concentration of all enzymes that 
                 catalyze the same intermediate step in the reaction
             modifier_reactants (:obj:`list` of :obj:`wc_lang.Species`): list of species 
                 in modifiers that should be included as reactants in the rate law    
-            
+
         Returns:
             :obj:`wc_lang.RateLawExpression`: rate law
             :obj:`list` of :obj:`wc_lang.Parameter`: list of parameters in the rate law     
@@ -164,38 +170,42 @@ def gen_mass_action_rate_law(avogadro, molecule_units, reaction, kinetic_paramet
         additional_reactants = modifier_reactants
 
     parameters = {}
-    parameters[avogadro.id] = avogadro
-    parameters[molecule_units.id] = molecule_units            
 
-    model_k = wc_lang.Parameter(id='k_{}'.format(reaction.id),
-                                    type=None,
-                                    units=unit_registry.parse_units('s^-1'),
-                                    value= kinetic_parameter_value),
+    avogadro = model.parameters.get_or_create(
+        id='Avogadro',
+        type=None,
+        value=scipy.constants.Avogadro,
+        units=unit_registry.parse_units('molecule mol^-1'))
+    parameters[avogadro.id] = avogadro
+
+    model_k = model.parameters.get_or_create(id='k_{}'.format(reaction.id),
+                                             type=None,
+                                             units=unit_registry.parse_units('s^-1'),
+                                             value=kinetic_parameter_value),
     parameters[model_k.id] = model_k
 
     expression_terms = []
     all_species = {}
     for species in reaction.get_reactants():
-                
+
         if species not in modifier_species or species in additional_reactants:
-        
+
             all_species[species.gen_id()] = species
             volume = species.compartment.init_density.function_expressions[0].function
-            
-            expression_terms.append(str(species.gen_id())) 
 
-    expression = '{} / {} * {} * {}'.format(
+            expression_terms.append(str(species.gen_id()))
+
+    expression = '{} * {} * {}'.format(
         model_k_cat.id,
-        molecule_units.id, 
-        ' * '.join([i.id for i in modifiers]), 
+        ' * '.join([i.id for i in modifiers]),
         ' * '.join(expression_terms))
-    
+
     rate_law_expression, error = wc_lang.RateLawExpression.deserialize(expression, {
         wc_lang.Parameter: parameters,
         wc_lang.Species: all_species,
         wc_lang.Observable: {i.id: i for i in modifiers},
         wc_lang.Function: {volume.id: volume},
-        })
+    })
     assert error is None, str(error)
 
-    return rate_law_expression, list(parameters.values())   
+    return rate_law_expression, list(parameters.values())